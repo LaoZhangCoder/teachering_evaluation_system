3f43e79957df8f3c67c0184282442267f6402a57
‘laozhang’ <1044985343@qq.com>
2020-01-03 17:53:34 +0800

    添加角色表

---
+++ teaching-evaluation-system-server/src/main/java/com/shendehaizi/model/RoleModel.java
@@ -0,0 +1,17 @@
+package com.shendehaizi.model;
+
+import lombok.Data;
+
+@Data
+public class RoleModel {
+    /**
+     * 角色Id
+     */
+    private Integer id;
+
+    /**
+     * 角色名称
+     */
+    private String roleName;
+
+}
